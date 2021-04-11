0c3e1dc1c2b6b81c71216928aba09e623fe70ef4
Sivan-H-A <sivananaf@gmail.com>
2021-04-11 22:43:44 +0300

    add Actor model

---
+++ src/model/ActorModel.js
@@ -0,0 +1,29 @@
+class ActorModel{
+    constructor(plainActor, lname, bday, img, imdb){
+        if(typeof fname==='object'){
+            this.fname = plainActor.fname;
+            this.lname = plainActor.lname;
+            this.bday = plainActor.bday;
+            this.img = plainActor.img;
+            this.imdb = plainActor.imdb;
+        } else{
+            this.fname = plainActor;
+            this.lname = lname;
+            this.bday = bday;
+            this.img = img;
+            this.imdb = imdb;
+        }
+        this.age = getAge(this.bday);
+    }
+
+    getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+}

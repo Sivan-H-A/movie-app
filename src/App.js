5e418e04078d46c92d7989199f19dc211673eab7
Sivan-H-A <sivananaf@gmail.com>
2021-04-12 00:00:24 +0300

    adding axios and actors.json file to read from

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import ActorPage from "./pages/ActorPage/ActorPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ActorModel from './model/ActorModel';
 
 function App() {
+  const [actors, setActors] = useState(null);
+
+  useEffect(()=>{
+    axios.get("actors.json").then(res=>{
+        console.log(res.data);
+        const newActors = res.data.map(plainActor => new ActorModel(plainActor));
+        setActors(newActors);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <ActorPage/>
+      <ActorPage actors={actors}/>
     </div>
   );
 }

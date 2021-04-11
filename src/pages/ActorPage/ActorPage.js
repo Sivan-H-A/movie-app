0a7c430d7677b260175294a04e52a1a5b4b208df
Sivan-H-A <sivananaf@gmail.com>
2021-04-12 00:56:35 +0300

    Adding call to Actors component delivering array of object as a prop

--- src/pages/ActorPage/ActorPage.js
+++ src/pages/ActorPage/ActorPage.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import HeaderActorPage from './HeaderActorPage'
+import HeaderActorPage from '../../components/HeaderActorPage/HeaderActorPage'
 import { Container } from 'react-bootstrap'
+import Actors from '../../components/Actors/Actors';
 
-export default function ActorPage() {
+export default function ActorPage({actors}) {
+    
     return (
         <Container>
             <HeaderActorPage/>
+            <Actors actors={actors}/>
         </Container>
     )
 }

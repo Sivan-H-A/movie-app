a8b5ee0e845e1dba8b5f00324d74b3f83ed774ce
Sivan-H-A <sivananaf@gmail.com>
2021-04-12 00:54:58 +0300

    create ActorCard with actor prop

---
+++ src/components/ActorCard/ActorCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import './ActorCard.css';
+
+export default function ActorCard({actor}) {
+    return (
+        <div className="col-sm-6 col-md-3 c-actor-card">
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src={actor.img}/>
+                <Card.Body>
+                    <Card.Title>
+                        <a href={actor.imdb} target="_blank">
+                            {`${actor.fname} ${actor.lname}`}
+                        </a>
+                    </Card.Title>
+                    <Card.Text>
+                        {actor.age}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}

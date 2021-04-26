4fd8d48c3c883e9913cfbf8aa07e6b1d5df668aa
Sivan-H-A <sivananaf@gmail.com>
2021-04-26 14:05:11 +0300

    adding additional functionality in homepage

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -5,7 +5,7 @@ import './NotFoundPage.css'
 export default function NotFoundPage() {
     return (
         <Container className="p-not-found">
-            <img src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png"></img>
+            <img src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png" alt=""></img>
         </Container>
     )
 }

95dca296797248355e0e53492ca7b0705447451e
Sivan-H-A <sivananaf@gmail.com>
2021-04-26 15:36:37 +0300

    change view for movie card

--- src/components/ActorCard/ActorCard.js
+++ src/components/ActorCard/ActorCard.js
@@ -13,7 +13,7 @@ export default function ActorCard({actor}) {
     return (
             <Card className="col-sm-6 col-md-3 c-actor-card" onClick={()=>setRedirectTo(actor.id)}>
                 <Card.Img variant="top" src={actor.img}/>
-                <Card.Body>
+                <Card.Body className="c-actor-card-body">
                     <Card.Title>
                         <a href={actor.imdb} target="_blank" rel="noreferrer">
                             {`${actor.fname} ${actor.lname}`}
